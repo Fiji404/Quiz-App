ede3d952e0371cb0af0cd348c182e3637acded40
Fiji <radek42310o@gmail.com>
2023-03-07 20:55:42 +0100

    update logo path

--- src/components/Nav/NavLogo.tsx
+++ src/components/Nav/NavLogo.tsx
@@ -1,7 +1,7 @@
 export const NavLogo = () => {
     return (
         <a href="/">
-            <img className="max-w-[50px] hover:drop-shadow-xl hover:-translate-y-1 transition-all" src="/public/quiz-logo.png" alt="cube with question mark" />
+            <img className="max-w-[50px] hover:drop-shadow-xl hover:-translate-y-1 transition-all" src="/quiz-logo.png" alt="cube with question mark" />
         </a>
     );
 };
